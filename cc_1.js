a6ebbb5d43356dba3a1f51590d4fa349a9cd38bd
Shamira Umstead <sumstead@usf.edu>
2024-05-17 16:44:32 -0400

    git add cc_1.js

--- cc_1.js
+++ cc_1.js
@@ -1,6 +1,6 @@
 
 // Initial logic for calculating tip using ternary operator
-const calcTip = (bill) => {bill >= 50 && bill <= 300 ? bill *0.15 : bill* 0.20; return tip;};
+const calcTip = (bill) => { const tip= bill >= 50 && bill <= 300 ? bill *0.15 : bill* 0.20; return tip;};
 
 // Add output functionality to display bill, tip, and total
 const displayBillTipTotal = (bill, tip) => {
@@ -9,9 +9,9 @@ const displayBillTipTotal = (bill, tip) => {
 };
 
 // Create and test calcTip function
+console. log("Test the function with a bill amount of $100");
+const testBill = 100;
+const tipAmount = calcTip(testBill);
+displayBillTipTotal(testBill, tipAmount)
 
-
-// Test the function with a bill amount of $100
-const billAmount = 100;
-const tipAmount = calculateTip(billAmount);
 console.log(`For a bill of $${billAmount}, the tip amount is $${tipAmount}.`); 
