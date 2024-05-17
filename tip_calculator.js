f990eaf23882616ec809d7887f45abed2cc5a956
Shamira Umstead <sumstead@usf.edu>
2024-05-17 16:15:39 -0400

    git add tip.js

---
+++ tip_calculator.js
@@ -0,0 +1,9 @@
+U78784426
+
+// Initial logic for calculating tip using ternary operator
+const calculateTip = (bill) => (bill >= 50 && bill <= 300) ? bill *0.15 : bill* 0.20;
+
+// Test the function with a bill amount of $100
+const billAmount = 100;
+const tipAmount = calculateTip(billAmount);
+console.log(`For a bill of $${billAmount}, the tip amount is $${tipAmount}.`);
