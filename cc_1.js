0a445f6484805286e0d61a9f45d0fb32fc5c6e08
Shamira Umstead <sumstead@usf.edu>
2024-05-18 16:51:55 -0400

    name changed

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+U78784426
 
 // Initial logic for calculating tip using ternary operator
 const calcTip = (bill) => { const tip= bill >= 50 && bill <= 300 ? bill *0.15 : bill* 0.20; return tip;};
@@ -15,21 +16,21 @@ const tipAmount = calcTip(testBill);
 displayBillDetails(testBill, tipAmount)
 console.log(`For a bill of $${testBill}, the tip amount is $${tipAmount}.`); 
 
-// Implement and populate arrats for bills, tips, and totals
-const billsSet1 = [275, 40, 430];
-const tipsSet1 = billsSet1.map(bill => calcTip(bill));
-const totalsSet1 = billsSet1.map((bill, index) => bill + tipsSet1[index]);
+// Implement and populate arrays for bills, tips, and totals
+const bills1 = [275, 40, 430];
+const tips1 = bills1.map(bill => calcTip(bill));
+const total1 = bills1.map((bill, index) => bill + tips1[index]);
 
 console.log("Data Set 1:");
-console.log("Bills:", billsSet1);
-console.log("Tips:", tipsSet1);
-console.log("Totals:", totalsSet1);
+console.log("Bills:", bills1);
+console.log("Tips:", tips1);
+console.log("Totals:", total1);
 
-const billsSet2 = [125, 555, 44];
-const tipsSet2 = billsSet2.map(bill => calcTip(bill));
-const totalsSet2 = billsSet2.map((bill, index) => bill + tipsSet2[index]);
+const bills2 = [125, 555, 44];
+const tips2 = bills2.map(bill => calcTip(bill));
+const total2 = bills2.map((bill, index) => bill + tips2[index]);
 
 console.log("Data Set 2:");
-console.log("Bills:", billsSet2);
-console.log("Tips:", tipsSet2);
-console.log("Totals:", totalsSet2); 
+console.log("Bills:", bills2);
+console.log("Tips:", tips2);
+console.log("Totals:", total2); 
